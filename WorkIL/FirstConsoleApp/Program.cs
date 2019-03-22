24ad24fed098ed3745fc220da7277affc03d3daf
Olivier Spinelli <olivier.spinelli@invenietis.com>
2019-03-22 15:53:16 +0100

    Work on Type: Value vs. Ref.

--- WorkIL/FirstConsoleApp/Program.cs
+++ WorkIL/FirstConsoleApp/Program.cs
@@ -3,34 +3,40 @@ using System.Text;
 
 namespace FirstConsoleApp
 {
+    class CUser
+    {
+        public string Name { get; }
+
+        public CUser( string name )
+        {
+            Name = name;
+        }
+
+        public override string ToString() => $" Class {Name}";
+    }
+
+    struct SUser
+    {
+        public string Name { get; }
+
+        public SUser( string name )
+        {
+            Name = name;
+        }
+        public override string ToString() => String.Format( " Struct {0}", Name );
+    }
+
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main( string[] args )
         {
+            var cu = new CUser( "Spi" );
+            var su = new SUser( "Spi" );
 
-#if NET461
-            Console.WriteLine( "NET461" );
-#else
-            Console.WriteLine( "PAS NET461" );
-#endif
 
-#if OPTIMIZED
-            Console.WriteLine( 0 );
-            Console.WriteLine( 1 );
-            Console.WriteLine( 2*2 );
-            Console.WriteLine( 3*3 );
-            Console.WriteLine( 4*4 );
-            Console.WriteLine( 5*5 );
-            Console.WriteLine( 6*6 );
-            Console.WriteLine( 6*6 );
-            Console.WriteLine( 7*7 );
-#else
-            for( int i = 0; i < 10; ++i )
-            {
-                Console.WriteLine( i*i );
-            }
-#endif
         }
+
     }
 
 }
