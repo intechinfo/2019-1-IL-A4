5ef15bf7a17c14268778673f188746e253a5c4b4
Olivier Spinelli <olivier.spinelli@invenietis.com>
2019-03-22 15:41:46 +0100

    DIsassembly with ILSpy.

--- WorkIL/FirstConsoleApp/Program.cs
+++ WorkIL/FirstConsoleApp/Program.cs
@@ -7,10 +7,29 @@ namespace FirstConsoleApp
     {
         static void Main(string[] args)
         {
-            string ss = "noël";
 
-            char c = ss[2];
-            var cat = Char.GetUnicodeCategory( c );
+#if NET461
+            Console.WriteLine( "NET461" );
+#else
+            Console.WriteLine( "PAS NET461" );
+#endif
+
+#if OPTIMIZED
+            Console.WriteLine( 0 );
+            Console.WriteLine( 1 );
+            Console.WriteLine( 2*2 );
+            Console.WriteLine( 3*3 );
+            Console.WriteLine( 4*4 );
+            Console.WriteLine( 5*5 );
+            Console.WriteLine( 6*6 );
+            Console.WriteLine( 6*6 );
+            Console.WriteLine( 7*7 );
+#else
+            for( int i = 0; i < 10; ++i )
+            {
+                Console.WriteLine( i*i );
+            }
+#endif
         }
     }
 
