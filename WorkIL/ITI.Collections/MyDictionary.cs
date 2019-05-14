f429cc299298370c076ae3f24ee5397dd951f8a1
Olivier Spinelli <olivier.spinelli@invenietis.com>
2019-05-14 10:13:38 +0200

    Add & indexer implemented.

--- WorkIL/ITI.Collections/MyDictionary.cs
+++ WorkIL/ITI.Collections/MyDictionary.cs
@@ -25,44 +25,68 @@ namespace ITI.Collections
 
         public int Count => _count;
 
-        public void Add( TKey key, TValue value )
+        public void Remove( TKey key )
         {
 
+        }
+
+        public void Add( TKey key, TValue value )
+        {
+            int h = key.GetHashCode();
+            int idx = h % _buckets.Length;
+            Node n = FindInBucket( key, idx );
+            if( n != null ) throw new InvalidOperationException();
+            else AddNewKeyValue( key, value, h, idx );
         }
 
         public TValue this[ TKey key ]
         {
             get
             {
-
+                int h = key.GetHashCode();
+                int idx = h % _buckets.Length;
+                Node n = FindInBucket( key, idx );
+                if( n != null ) return n.Value;
+                throw new KeyNotFoundException();
             }
             set
             {
                 int h = key.GetHashCode();
                 int idx = h % _buckets.Length;
-                Node n = _buckets[idx];
-                while( n != null )
+                Node n = FindInBucket( key, idx );
+                if( n != null )
                 {
-                    if( n.Key.Equals( key ) )
-                    {
-                        n.Value = value;
-                        return;
-                    }
-                    n = n.Next;
+                    n.Value = value;
                 }
-                int fillFactor = _count / _buckets.Length;
-                if( fillFactor > 20 )
-                {
-                    Grow();
-                    idx = h % _buckets.Length;
-                }
-                _count++;
-                _buckets[idx] = new Node( key )
-                {
-                    Value = value,
-                    Next = _buckets[idx]
-                };
+                else AddNewKeyValue( key, value, h, idx );
             }
+        }
+
+        void AddNewKeyValue( TKey key, TValue value, int hashCode, int currentIdx )
+        {
+            int fillFactor = _count / _buckets.Length;
+            if( fillFactor > 20 )
+            {
+                Grow();
+                currentIdx = hashCode % _buckets.Length;
+            }
+            _count++;
+            _buckets[currentIdx] = new Node( key )
+            {
+                Value = value,
+                Next = _buckets[currentIdx]
+            };
+        }
+
+        Node FindInBucket( TKey key, int idx )
+        {
+            Node n = _buckets[idx];
+            while( n != null )
+            {
+                if( n.Key.Equals( key ) ) break;
+                n = n.Next;
+            }
+            return n;
         }
 
         void Grow()
