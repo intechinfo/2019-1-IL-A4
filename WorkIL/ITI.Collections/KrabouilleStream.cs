8e7af8927820bbe25245aca9fb43604fbd0db6f6
Olivier Spinelli <olivier.spinelli@invenietis.com>
2019-05-16 11:30:44 +0200

    KrabouilleStream (just a proxy right now).

---
+++ WorkIL/ITI.Collections/KrabouilleStream.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ITI.Collections
+{
+    public class KrabouilleStream : Stream
+    {
+        readonly Stream _inner;
+
+        public KrabouilleStream( Stream innerStream )
+        {
+            _inner = innerStream;
+        }
+
+        /// <summary>
+        /// Gets the inner stream.
+        /// </summary>
+        public Stream InnerStream => _inner;
+
+        public override bool CanRead => _inner.CanRead;
+
+        public override bool CanWrite => _inner.CanWrite;
+
+        public override bool CanSeek => _inner.CanSeek;
+
+        public override long Length => _inner.Length;
+
+        public override long Position
+        {
+            get => _inner.Position;
+            set => _inner.Position = value;
+        }
+
+        public override void Flush()
+        {
+            _inner.Flush();
+        }
+
+        public override int Read( byte[] buffer, int offset, int count )
+        {
+            return _inner.Read( buffer, offset, count );
+        }
+
+        public override long Seek( long offset, SeekOrigin origin )
+        {
+            return _inner.Seek( offset, origin );
+        }
+
+        public override void SetLength( long value )
+        {
+            _inner.SetLength( value );
+        }
+
+        public override void Write( byte[] buffer, int offset, int count )
+        {
+            _inner.Write( buffer, offset, count );
+        }
+    }
+}
