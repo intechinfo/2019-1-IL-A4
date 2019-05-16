e6dc5c1ae14260b7b1968f50ae2f4b066256664f
Olivier Spinelli <olivier.spinelli@invenietis.com>
2019-05-16 11:44:28 +0200

    Basic tests.

--- WorkIL/SimpleUnitTests/StreamTests.cs
+++ WorkIL/SimpleUnitTests/StreamTests.cs
@@ -42,5 +42,57 @@ namespace SimpleUnitTests
                 while( len == buffer.Length );
             }
         }
+
+
+        [Test]
+        public void krabouille_works()
+        {
+            var origin = ThisFilePath();
+            var crypted = origin + ".krab";
+            var restored = crypted + ".clear";
+
+            DoKrabouille( origin, crypted, "Secret..." );
+
+            File.ReadAllBytes( origin )
+                .SequenceEqual( File.ReadAllBytes( crypted ) )
+                .Should().BeFalse();
+
+            DoUnkrabouille( crypted, restored, "Secret..." );
+
+            File.ReadAllBytes( origin )
+                .SequenceEqual( File.ReadAllBytes( restored ) )
+                .Should().BeTrue();
+        }
+
+        [Test]
+        public void krabouille_requires_the_passphrase()
+        {
+            var origin = ThisFilePath();
+            var crypted = origin + ".krab";
+            var failed = crypted + ".failed";
+
+            DoKrabouille( origin, crypted, "Secret1" );
+
+            File.ReadAllBytes( origin )
+                .SequenceEqual( File.ReadAllBytes( crypted ) )
+                .Should().BeFalse();
+
+            DoUnkrabouille( crypted, failed, "Secret2" );
+
+            File.ReadAllBytes( origin )
+                .SequenceEqual( File.ReadAllBytes( failed ) )
+                .Should().BeFalse();
+        }
+
+
+        void DoKrabouille( string origin, string crypted, string passPhrase )
+        {
+            throw new NotImplementedException();
+        }
+
+        void DoUnkrabouille( string crypted, string restored, string passPhrase )
+        {
+            throw new NotImplementedException();
+        }
     }
 }
