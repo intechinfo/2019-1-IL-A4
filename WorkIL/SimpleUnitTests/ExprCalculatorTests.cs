608d951bb48fe337d19e33e9ac72748cc716409a
Olivier Spinelli <olivier.spinelli@invenietis.com>
2019-06-13 11:18:27 +0200

    Created tests for ternary.

--- WorkIL/SimpleUnitTests/ExprCalculatorTests.cs
+++ WorkIL/SimpleUnitTests/ExprCalculatorTests.cs
@@ -12,13 +12,23 @@ namespace SimpleUnitTests
     {
         [TestCase( "3*5", 3.0 * 5 )]
         [TestCase( "3+5", 3.0 + 5 )]
-        [TestCase( " 3  *  (  2  +  2  )  ", 3.0 * (2+2) )]
+        [TestCase( " 3  *  (  2  +  2  )  ", 3.0 * (2 + 2) )]
         [TestCase( "3 + 5 * 125 / 7 - 6 + 10", 3.0 + 5 * 125 / 7.0 - 6 + 10 )]
         [TestCase( "7 - 6 + 10", 7.0 - 6 + 10 )]
         [TestCase( "7 - -2", 7.0 - -2.0 )]
         [TestCase( "7 + -2", 7.0 + -2.0 )]
         [TestCase( "7 * -(5+2*3)", 7.0 * -(5 + 2 * 3.0) )]
         public void test_calculator( string expression, double expected )
+        {
+            ExprCalculator.Compute( expression ).Should().Be( expected );
+        }
+
+        [TestCase( "3+5 ? 80 : -6", 80.0 )]
+        [TestCase( "3*-5 ? 8 : -6", -6.0 )]
+        [TestCase( "-1 ? 2 ? 3 : 4 : 5", 5.0 )]
+        [TestCase( "-1 ? 2 ? 3 : 4 : 5 ? 6 : 7", 7.0 )]
+        [TestCase( "1 ? 2 ? 3 : 4 : 5 ? 6 : 7", 4.0 )]
+        public void test_conditional( string expression, double expected )
         {
             ExprCalculator.Compute( expression ).Should().Be( expected );
         }
